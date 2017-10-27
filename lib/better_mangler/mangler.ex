3852020819438c82ed18f85c11740228f340c6ab
Patrick Carver <patrickcarver@gmail.com>
2017-10-26 19:58:55 -0500

    parse adjective file into list

--- lib/better_mangler/mangler.ex
+++ lib/better_mangler/mangler.ex
@@ -1,9 +1,11 @@
 defmodule Mangler do
+	alias Poison
+
 	def run(word) do
 		"../../assets/adjectives.json"
 		|> Path.expand(__DIR__)
 		|> File.read!()
-		
-		
+		|> Poison.decode!()
+		|> Map.fetch("adjectives")
 	end
 end
