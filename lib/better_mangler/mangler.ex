0787d4c565fdc3c0f3357d57b2fe1a07cb7da792
Patrick Carver <patrickcarver@gmail.com>
2017-10-26 23:28:06 -0500

    checking

--- lib/better_mangler/mangler.ex
+++ lib/better_mangler/mangler.ex
@@ -2,6 +2,38 @@ defmodule BetterMangler.Mangler do
 	alias BetterMangler.WordService
 
 	def run(word) do
-		WordService.get_random(:adjective)
+		word_length = String.length(word)
+
+		cond do
+			word_length > 10 ->
+				{ :error, "Please, no words/acronyms greater than 10 characters." }
+			word_length == 0 ->
+				{ :error, "The word/acronym is empty" }
+			:true ->
+				word |> mangle
+		end
 	end
+
+	defp mangle(word) do
+		template = word |> String.length |> get_template
+
+		word 
+		|> String.codepoints
+	end
+
+	defp get_template(length) do
+		template = %{
+			1 => [],
+			2 => [],
+			3 => [{:noun, :verb, :adverb},{:adjective, :noun, :verb}],
+			4 => [],
+			5 => [],
+			6 => [],
+			7 => [],
+			8 => [],
+			9 => [],
+			10 => []
+		}
+	end
+
 end
