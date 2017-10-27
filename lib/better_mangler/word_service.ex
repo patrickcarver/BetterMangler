497f2e4e9b25ada5e7f5037dcb7217ab078162b7
Patrick Carver <patrickcarver@gmail.com>
2017-10-26 23:13:48 -0500

    Handle empty word or word more than 10 chars

--- lib/better_mangler/word_service.ex
+++ lib/better_mangler/word_service.ex
@@ -2,11 +2,24 @@ defmodule BetterMangler.WordService do
 	alias Poison
 
 	def get_random(type) when type in [:adjective, :adverb, :noun, :verb] do
-		type |> load_words() |> Enum.random() 
+		type 
+		|> load_words() 
+		|> Enum.random()
+		|> get_tense_if_verb(type) 
 	end
 
 	def get_random(_) do
 		{ :error, "Not a recognized option" }
+	end
+
+	defp get_tense_if_verb(map, type) when type == :verb do
+		tense = ["past", "present"] |> Enum.random()
+		{ :ok, verb } = map |> Map.fetch(tense)
+		verb		
+	end
+
+	defp get_tense_if_verb(word, type) when type in [:adjective, :adverb, :noun] do
+		word
 	end
 
 	defp load_words(type) do
