017160ef0b653fff78c3654024daee79b62330f7
Patrick Carver <patrickcarver@gmail.com>
2017-11-02 22:46:12 -0500

    Put templates in separate module

--- lib/better_mangler/mangler.ex
+++ lib/better_mangler/mangler.ex
@@ -1,50 +1,6 @@
 defmodule BetterMangler.Mangler do
-    alias BetterMangler.WordService
+    alias BetterMangler.{WordService, Templates}
     alias Inflex
-
-    @templates %{
-        1 => [
-                [:noun], 
-                [:verb], 
-                [:adjective], 
-                [:adverb]
-             ],
-        2 => [
-                [:noun, :verb], 
-                [:adjective, :noun], 
-                [:verb, :adjective],
-                [:noun, :noun]
-             ],
-        3 => [
-                [:noun, :verb, :adverb], 
-                [:adjective, :noun, :verb], 
-                [:noun, :verb, :noun],
-                [:noun, :noun, :noun]
-             ],
-        4 => [
-                [:adjective, :noun, :verb, :adverb],
-                [:noun, :noun, :noun, :noun]
-             ],
-        5 => [
-                [:adverb, :adjective, :noun, :verb, :noun],
-                [:adjective, :noun, :verb, :adjective, :noun]
-             ],
-        6 => [
-                [:adverb, :adjective, :noun, :verb, :adjective, :noun]
-             ],
-        7 => [
-                [:adverb, :adjective, :noun, :adverb, :verb, :adjective, :noun]
-             ],
-        8 => [
-                [:adverb, :adjective, :noun, :verb, :adverb, :adjective, :noun]
-             ],
-        9 => [
-                [:adverb, :adverb, :adjective, :noun, :noun, :adverb, :adjective, :verb, :adverb]
-             ],
-        10 => [
-                [:adverb, :adverb, :adverb, :adjective, :noun, :noun, :adverb, :adjective, :verb, :adverb] 
-              ]
-    }
 
     def run(word) do
         word_length = String.length(word)
@@ -60,7 +16,7 @@ defmodule BetterMangler.Mangler do
     end
 
     defp mangle(word) do
-        template = word |> String.length |> get_template
+        template = word |> String.length |> Templates.get_random_template_by_length
         letters = String.codepoints(word)
 
         process([], template, letters)
@@ -127,10 +83,4 @@ defmodule BetterMangler.Mangler do
     defp verb?(item) do
         Enum.at(item, 0) == :verb
     end
-
-    defp get_template(length) do
-        {:ok, list} = Map.fetch(@templates, length)
-        Enum.random(list)
-    end
-
 end
